94ddeeab49decaf59c1ab048dacb213360fe9568
Grant Petersen-Speelman <grantspeelman@users.noreply.github.com>
2024-09-12 16:14:16 +1000

    use odata standardize error format and add oas2 for create (#9)
    * using standard error format allows PowerBI to understand the error and
    display it back to the user if anything goes wrong
    * update oas2 definitions to reference standard error format
    * add create definition on oas2

--- lib/odata_duty/errors.rb
+++ lib/odata_duty/errors.rb
@@ -3,14 +3,41 @@ module OdataDuty
 
   class PropertyAlreadyDefinedError < ArgumentError; end
 
-  class ServerError < Error; end
-  class NoImplementationError < ServerError; end
+  class RequestError < Error
+    def initialize(message, code: nil, target: nil)
+      super(message)
+      @code = code
+      @target = target
+    end
+
+    def status
+      :internal_server_error
+    end
+  end
+
+  class ServerError < RequestError; end
+
+  class NoImplementationError < ServerError
+    def status
+      :not_implemented
+    end
+  end
+
   class NotYetSupportedError < NoImplementationError; end
   class InvalidValue < ServerError; end
 
-  class ClientError < Error; end
+  class ClientError < RequestError
+    def status
+      :bad_request
+    end
+  end
 
-  class ResourceNotFoundError < ClientError; end
+  class ResourceNotFoundError < ClientError
+    def status
+      :not_found
+    end
+  end
+
   class UnknownPropertyError < ClientError; end
   class UnknownCollectionError < ClientError; end
   class InvalidFilterValue < ClientError; end
--- lib/odata_duty/oas2.rb
+++ lib/odata_duty/oas2.rb
@@ -2,6 +2,7 @@ module OdataDuty
   class OAS2
     def self.build_json(schema)
       builder = new(schema)
+      builder.add_error_definition
       builder.add_enum_definitions
       builder.add_complex_definitions
       builder.add_collection_paths
@@ -20,6 +21,24 @@ module OdataDuty
       hash['info']['title'] = schema.title if schema.title
     end
 
+    ERROR_PROPERTIES = {
+      'code' => { 'type' => 'string', 'description' => 'A service-defined error code.' },
+      'message' => { 'type' => 'string', 'description' => 'A human-readable message.' },
+      'target' => { 'type' => 'string', 'description' => 'The target of the error.',
+                    'x-nullable' => true }
+    }.freeze
+    def add_error_definition
+      hash['definitions']['Error'] = {
+        'type' => 'object',
+        'properties' => {
+          'error' => {
+            'type' => 'object',
+            'properties' => ERROR_PROPERTIES
+          }
+        }
+      }
+    end
+
     def add_enum_definitions
       schema.enum_types.each do |enum_type|
         hash['definitions'][enum_type.name] = enum_type.to_oas2
@@ -35,7 +54,8 @@ module OdataDuty
     def add_collection_paths
       schema.collection_entity_sets.each do |entity_set|
         hash['paths']["/#{entity_set.url}"] = {
-          'get' => entity_set.collection.to_oas2
+          'get' => entity_set.collection.to_oas2,
+          'post' => CollectionPostPath.to_oas2(entity_set)
         }
       end
     end
@@ -45,6 +65,53 @@ module OdataDuty
         hash['paths']["/#{entity_set.url}({id})"] = {
           'get' => entity_set.individual.to_oas2
         }
+      end
+    end
+
+    class CollectionPostPath
+      def self.to_oas2(entity_set)
+        path_info = new(entity_set)
+        {
+          'operationId' => path_info.operation_id,
+          'produces' => path_info.produces,
+          'parameters' => path_info.parameters,
+          'responses' => path_info.responses
+        }
+      end
+
+      def initialize(entity_set)
+        @entity_set = entity_set
+      end
+
+      def operation_id
+        "Create#{@entity_set.name}"
+      end
+
+      def produces
+        ['application/json']
+      end
+
+      def parameters
+        [
+          {
+            'name' => 'body', 'in' => 'body', 'required' => true, 'schema' => entity_type_schema
+          }
+        ]
+      end
+
+      def responses
+        {
+          '200' => { 'description' => 'Success', 'schema' => entity_type_schema },
+          '201' => { 'description' => 'Created', 'schema' => entity_type_schema },
+          'default' => { 'description' => 'Unexpected error',
+                         'schema' => { '$ref' => '#/definitions/Error' } }
+        }
+      end
+
+      private
+
+      def entity_type_schema
+        { '$ref' => "#/definitions/#{@entity_set.entity_type.name}" }
       end
     end
   end
